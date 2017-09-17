2e56471fe53cb031a7961473024b8eb676dfbb28
Fernando Rosendo <fernandolgl75@gmail.com>
2017-09-16 21:17:56 -0300

    Final

--- blackjack.py
+++ blackjack.py
@@ -3,14 +3,14 @@ import os
 import sys
 
 '''
-This Blackjack project is supposed to be basic, including only 'Hit' and 'Stand'. Though I'm increasing the complexity of it by handling files.
+This Blackjack project is supposed to be basic, including only 'Hit' and 'Stand'.
 '''
+
 values = {'A': 11, '2': 2, '3': 3, '4': 4, '5': 5, '6': 6, '7': 7, '8': 8, '9': 9, 'T': 10, 'J': 10, 'K': 10, 'Q': 10}
 suits = ['S', 'D', 'C', 'H']
 
 keys_values = list(values.keys())  # variable that defines all the values a card can have (list)
 deck = [card_value + suit for card_value in keys_values for suit in suits]  # all the cards possible (list)
-# shuffle(deck)  I don't need to shuffle the deck now because everytime a player hits, it's shuffled.
 
 # Now the required classes for the logic.
 
@@ -57,17 +57,15 @@ class Hand(object):
 
 class Player(object):
 
-    def __init__(self, name='', score=0, hand=None):
-        self.name = name
-        self.score = score
+    def __init__(self, hand=None):
         self.hand = hand if hand is not None else Hand()
 
-    def hit(self, pos=0):
+    def hit(self):
         '''
         Adds a card to the player's hand
         '''
         shuffle(deck)
-        self.hand.add_card(deck[pos])
+        self.hand.add_card(deck[0])
 
     def get_hand(self):
         return self.hand
@@ -88,11 +86,9 @@ def clear_screen():
 
 
 def game():
-    global name
-
     # Creating the two Player objects
 
-    player = Player(name)
+    player = Player()
     dealer = Player()
     # Dealing the first 2 cards the player and 1 card to the dealer
 
@@ -129,32 +125,33 @@ def game():
             game()
         elif option.lower() == 'n':
             clear_screen()
-            while True:
-                print("Return to the main menu?(Y)es or (N)o?")
-                option = input()
-                clear_screen()
-                if option.lower() == 'y':
-                    menu()
-                elif option.lower() == 'n':
-                    return
-                else:
-                    print("Please say a valid option.")
-                    continue
+            return
         else:
             clear_screen()
             print("Invalid, please say 'y' or 'n'")
             play_again()
-        # main loop
+
+    player.get_hand().count()
+    if player.get_hand().get_value() == 21:
+        clear_screen()
+        print_info()
+        print("Congratulations! You got blackjack!")
+        play_again()
+        return
+
+    # main loop
     while True:
 
         while True:
             clear_screen()
             print_info()
-            print("Do you wish to 'stand' or to 'hit'?")
+            print("Do you wish to (s)tand or to (h)it?")
             option = input()
-            if option.lower() == 'stand':
+            if option.lower() in ['s', 'stand']:
                 # If the player stood while he was losing, it means the player gave up.
                 if dealer.get_hand().get_value() > player.get_hand().get_value():
+                    clear_screen()
+                    print_info()
                     print("You gave up!")
                     break
                 # If the player is standing, all there's left to do is deal cards to the dealer and stop when the value of the dealer's hand is greater than (or equal to, if the player's hand value is 21, resulting in a draw) the player's hand value.
@@ -174,7 +171,7 @@ def game():
                         break
                     # The AI doesn't want a draw unless there's no choice! So it won't stop hitting unless it's a 21-to-21 draw.
                 break
-            elif option.lower() == 'hit':
+            elif option.lower() in ['h', 'hit']:
                 player.hit()
                 player.get_hand().count()
                 clear_screen()
@@ -187,44 +184,4 @@ def game():
         return
 
 
-def menu():
-
-    # Welcome
-    clear_screen()
-    print("\tWelcome to Blackjack")
-    print("\n\n1-Play\n2-View Scores\n3-Quit")
-
-    # Checking the option value
-    while True:
-        try:
-            option = int(input())
-        except:
-            print("Please type in a valid integer.")
-            continue
-        if option not in [1, 2, 3]:
-            print("Please say a valid option.")
-            continue
-        break
-
-    # if/elif/else chain for the option.
-    if option == 1:
-        game()
-    elif option == 2:
-        pass
-    elif option == 3:
-        return
-
-
-# Beginning
-clear_screen()
-print("Hey, welcome to the game. What's your name?")
-while True:
-    name = input()
-    if len(name.split()) == 0:
-        clear_screen()
-        print("Nice empty name! Try again please.")
-        continue
-    break
-
-
-menu()
+game()
